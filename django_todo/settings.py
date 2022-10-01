f0defd0840083e819f8a95182de33dde14c4ce15
marked-gil <markgil_creates@yahoo.com>
2022-10-01 09:35:01 +0000

    Fix allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@@3_v=iisq@*t@d(7+4x5p1o_wh4%9oa_a)jbezhlm*$jnp49o'
+SECRET_KEY = os.environ.get("SECRET_KEY", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['my-hellodjango.herokuapp.com']
 
 
 # Application definition
